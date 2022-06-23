665f954e6728cfeeff2751a9f4a23bad6c3fdddf
Negin Zahedi <zahedi.negin@gmail.com>
2022-06-22 21:04:21 -0400

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Negin Zahedi on 2022-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
